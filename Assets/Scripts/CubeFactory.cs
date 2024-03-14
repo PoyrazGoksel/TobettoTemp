961a1bdd22e056777c60f2abf497af2aaf1a751d
PoyrazGoksel <poyraz_goksel@hotmail.com>
2024-03-14 17:55:11 +0300

    physics detached from transform

--- Assets/Scripts/CubeFactory.cs
+++ Assets/Scripts/CubeFactory.cs
@@ -11,20 +11,13 @@ public class CubeFactory : MonoBehaviour
     [SerializeField] private int _xSize;
     [SerializeField] private int _zSize;
     [SerializeField] private float _moveSpeed;
-    
-    private List<Vector2Int> _spawnLocStack = new();
+
+    private void Awake() {}
 
     private void Start()
     {
-        _spawnLocStack.Add(new Vector2Int(0, 0));
         StartCoroutine(CubeRainRoutine());
         StartCoroutine(InputListenerRoutine());
-    }
-
-    private void InstantiateCube(Vector3 newCubePos)
-    {
-        GameObject newObj = Instantiate(_cubePrefab, transform);
-        newObj.transform.localPosition = newCubePos;
     }
 
     private IEnumerator InputListenerRoutine()
@@ -39,7 +32,7 @@ public class CubeFactory : MonoBehaviour
                 currPos.z += moveDelta;
                 transform.position = currPos;
             }
-            
+
             if(Input.GetKey(KeyCode.S))
             {
                 Vector3 currPos = transform.position;
@@ -53,51 +46,38 @@ public class CubeFactory : MonoBehaviour
                 currPos.x += moveDelta;
                 transform.position = currPos;
             }
-            
+
             if(Input.GetKey(KeyCode.A))
             {
                 Vector3 currPos = transform.position;
                 currPos.x -= moveDelta;
                 transform.position = currPos;
             }
-            
-            
+
             yield return null;
         }
     }
 
-    private void Awake() {}
-
     private IEnumerator CubeRainRoutine()
     {
-        int lastX = 0;
-        int lastZ = 0;
-
         while(true)
         {
+            Vector3 currPos = transform.position;
+            
             int randX = Random.Range(0, _xSize);
             int randZ = Random.Range(0, _zSize);
 
-            if(lastX == randX && lastZ == randZ)
-            {
-                randX = _spawnLocStack[^1].x;
-                randZ = _spawnLocStack[^1].y;
-            }
-            
-            Vector3 newCubePos = new(randX, transform.position.y, randZ);
+            Vector3 newCubePos = new(currPos.x + randX, currPos.y, currPos.z + randZ);
 
-            _spawnLocStack.Add(new Vector2Int(randX, randZ));
-
-            if(_spawnLocStack.Count > _index)
-            {
-                _spawnLocStack.Remove(_spawnLocStack[_index]);
-            }
-            
             InstantiateCube(newCubePos);
 
-            lastX = randX;
-            lastZ = randZ;
             yield return new WaitForSeconds(_seconds);
         }
+    }
+
+    private void InstantiateCube(Vector3 newCubePos)
+    {
+        GameObject newObj = Instantiate(_cubePrefab);
+        newObj.transform.position = newCubePos;
     }
 }
